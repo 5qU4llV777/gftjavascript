f98e58c7f6728ed483e9470e9efec998609e5646
5qU4llV777 <166067521+5qU4llV777@users.noreply.github.com>
2024-09-30 15:39:36 -0400

    Add files via upload

--- desafios de codigo.js
+++ desafios de codigo.js
@@ -1,4 +1,4 @@
-// desafios dio
+// desafios dio 1
 // Para converter os valores inseridos na entrada, utilizamos a função JavaScript parseInt() que converte a parte inicial da string em um número inteiro.
 const posicaoInicial = parseInt(gets());
 const totalPassos = parseInt(gets());
@@ -10,7 +10,7 @@ const posicaoFinal = posicaoInicial + totalPassos;
 print("Posicao final do heroi:", posicaoFinal);
 
 ////////////////////////////////////////////
-
+// desafio 2
 let num1 = parseInt(gets());
 let num2 = parseInt(gets());
 
@@ -19,3 +19,26 @@ const xpGanho = num1 * num2 * 100  ;
 
 // Imprime a quantidade de XP ganho
 print("Voce ganhou " + xpGanho + " XP!");
+
+
+//////////////////////////////////////////////////
+// desafio 3
+
+let escolhaDoTreinador = parseInt(gets());
+let pokemonEscolhido; 
+
+// Implementa as condições necessárias para a escolha do Pokémon
+if (escolhaDoTreinador === 1) {
+    pokemonEscolhido = "Bulbasaur";
+} else if (escolhaDoTreinador === 2) {
+    pokemonEscolhido = "Charmander";
+} else if (escolhaDoTreinador === 3) {
+    pokemonEscolhido = "Pikachu";
+} else {
+    pokemonEscolhido = "Mewtwo"; // Pokémon default para escolha inválida
+}
+
+// Imprime o Pokémon escolhido
+if (pokemonEscolhido) {
+    print("Voce escolheu o " + pokemonEscolhido + " como seu Pokemon inicial.");
+}
